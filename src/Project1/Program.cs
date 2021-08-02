1317c80d16bbe302f0cd98251e7fc5b01005f86f
gsdr1992 <gabriel_silveira_r@hotmail.com>
2021-08-01 22:54:47 -0300

    First commit

---
+++ src/Project1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace Project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var connString = "mongodb+srv://m220student:<m220password>@mflix.5nnhc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+            var client = new MongoClient(connString);
+            var db = client.GetDatabase("sample_flix");
+            var collection = db.GetCollection<BsonDocument>("movies");
+
+
+            var result = collection.Find(new BsonDocument())
+                           .SortByDescending(m => m["runtime"])
+                           .Limit(10)
+                           .ToList();
+
+            Console.WriteLine(result);
+        }
+    }
+}
